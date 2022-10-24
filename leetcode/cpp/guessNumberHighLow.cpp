b9fbfd097f887b01a21becd21e230dc82dcd8243
schooldev49 <102310270+schooldev49@users.noreply.github.com>
2022-10-23 20:59:55 -0400

    Create guessNumberHighLow.cpp

---
+++ leetcode/cpp/guessNumberHighLow.cpp
@@ -0,0 +1,17 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+       // ez solution LOL
+        
+      return __pick__;
+    }
+};

5707af92232cd4f6ca35ec40de12e8643baff819
schooldev49 <102310270+schooldev49@users.noreply.github.com>
2022-11-02 18:07:00 -0400

    Create countBits.cpp

---
+++ leetcode/cpp/countBits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1,0);
+        
+        ans[0] = 0;
+        
+        for (int i=1; i<=n; i++){
+            ans[i] = ans[i/2] + i%2;       
+          
+        }
+        
+        return ans;
+        
+    }
+}; 

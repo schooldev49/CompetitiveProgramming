256ec5cff3ef0cec6868ffc74f3e3cfd53e0b95a
schooldev49 <102310270+schooldev49@users.noreply.github.com>
2022-11-11 18:02:09 -0500

    Update struct.c

--- dataStructures/c/struct.c
+++ dataStructures/c/struct.c
@@ -1,8 +1,17 @@
+#include <string.h>
+
 typedef struct {
   int age;
   string name;
 }
 person
 
+int main(void){
+ person Person[2];
 
-person Person[2];
+ person[0].age = 69;
+ person[0].name = "Bob";
+ 
+ person[1].age = 420;
+ person[1].name = "Bob++";
+}
